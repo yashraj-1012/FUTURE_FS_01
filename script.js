5b13cc8ed721b547ef72f99d51ec540fcdb34293
yashraj-1012 <yashrajmaddi00@gamil.com>
2025-07-24 08:19:06 +0530

    updated navigations

--- script.js
+++ script.js
@@ -28,3 +28,57 @@ document.addEventListener("DOMContentLoaded", function () {
 //   link.click();
 //   document.body.removeChild(link);
 // });
+
+/*skills info container */
+
+// function openModal(skill) {
+//   const modal = document.getElementById("modal");
+//   const backdrop = document.getElementById("modal-backdrop");
+//   const title = document.getElementById("modal-title");
+//   const content = document.getElementById("modal-content");
+
+// Customize based on skill
+//   if (skill === "Dart") {
+//     title.textContent = "Dart";
+//     content.textContent =
+//       "Dart is a client-optimized language for fast apps on any platform, especially used with Flutter.";
+//   }
+
+//   if (skill === "Flutter") {
+//     title.textContent = "Flutter";
+//     content.textContent =
+//       "Flutter is an open-source UI toolkit by Google for building beautiful, natively compiled apps.";
+//   }
+
+//   if (skill === "Java") {
+//     title.textContent = "Flutter";
+//     content.textContent =
+//       "Flutter is an open-source UI toolkit by Google for building beautiful, natively compiled apps.";
+//   }
+
+//   if (skill === "JavaScript") {
+//     title.textContent = "Flutter";
+//     content.textContent =
+//       "Flutter is an open-source UI toolkit by Google for building beautiful, natively compiled apps.";
+//   }
+
+//   if (skill === "HTML") {
+//     title.textContent = "Flutter";
+//     content.textContent =
+//       "Flutter is an open-source UI toolkit by Google for building beautiful, natively compiled apps.";
+//   }
+
+//   if (skill === "CSS") {
+//     title.textContent = "Flutter";
+//     content.textContent =
+//       "Flutter is an open-source UI toolkit by Google for building beautiful, natively compiled apps.";
+//   }
+
+//   modal.style.display = "block";
+//   backdrop.style.display = "block";
+// }
+
+// function closeModal() {
+//   document.getElementById("modal").style.display = "none";
+//   document.getElementById("modal-backdrop").style.display = "none";
+// }
